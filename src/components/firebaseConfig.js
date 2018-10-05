e5919eee2ab0c8508fc44567b805aa7e08c558b3
Martin Brehm <maddin@martin.fritz.box>
2018-10-05 15:26:47 +0200

    adding all files but dist

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAuUipBNyaGMG_QW99aC8lIznLF5YnmHfI",
+    authDomain: "vnshoptest.firebaseapp.com",
+    databaseURL: "https://vnshoptest.firebaseio.com",
+    projectId: "vnshoptest",
+    storageBucket: "vnshoptest.appspot.com",
+    messagingSenderId: "715849988597"
+}
