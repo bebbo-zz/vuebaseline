292a929b256ed27bffad7c2f7cfc258a92d39dca
bebbo-zz <martin.brehm@web.de>
2018-10-06 09:44:37 -0400

    display file added

---
+++ src/components/Display.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <p>Hier sind wir richtig</p>
+    <img v-bind:src="imageLink" height="480" width="480" />
+</div>
+</template>
+
+<script>
+//import db from './firebaseInit'
+export default {
+    name: 'display',
+    data () {
+        return {
+            imageLink: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png'            
+        }
+    }
+    //  https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png
+}
+</script>
+    
